d07a0e4a987aa62afd7b0c9398e96577922db972
Natalia Galan <n8galan@gmail.com>
2020-07-01 00:30:50 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,17 @@
+#build a method, square_array,
+#that squares each element in an array of numbers
+#and returns a new array of these squared numbers.
+require 'pry'
+
 def square_array(array)
   # your code here
+  index = 0
+  squared_array = []
+  while  index < array.length do
+    squared_element =  array[index] ** 2
+    squared_array << squared_element
+    index += 1
+  end
+  squared_array
+  binding.pry
 end
